50f2d6c962bb491061fc220ec9b85cf8f6de27a2
Dakota Justin <dakotajustin94@gmail.com>
2018-01-31 10:52:01 -0500

    Click event for journal item

--- business/src/main/java/mvvm/com/business/viewmodel/JournalEntryViewModel.kt
+++ business/src/main/java/mvvm/com/business/viewmodel/JournalEntryViewModel.kt
@@ -2,9 +2,12 @@ package mvvm.com.business.viewmodel
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
 import mvvm.com.business.model.JournalEntry
 
 class JournalEntryViewModel(journalEntry: JournalEntry) : ViewModel() {
+
+    private val Tag = "JournalEntryVM"
 
     val title = MutableLiveData<String>()
     val content = MutableLiveData<String>()
@@ -12,6 +15,10 @@ class JournalEntryViewModel(journalEntry: JournalEntry) : ViewModel() {
     init {
         this.title.postValue(journalEntry.title)
         this.content.postValue(journalEntry.content)
+    }
+
+    fun journalItemClicked() {
+       Log.v(Tag, "Journal Item Clicked")
     }
 
 
